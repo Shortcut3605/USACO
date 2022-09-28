719ca3caabfc101f0e765ab2cf0490d6905d6ed2
Shortcut3605 <breadzhacks@gmail.com>
2022-09-28 19:10:26 -0400

    test cases 7 and 10 are wrong

--- lostcow/lostcow.py
+++ lostcow/lostcow.py
@@ -1,23 +1,32 @@
+from pickle import FALSE
+
+
 f = open("lostcow.in", "r")
 w = open("lostcow.out","w")
 
 x,y = map(int, f.readline().strip().split())
+if x == y: 
+    print(0, file=w)
+    f.close()
+    w.close()
+    exit()
+    
 ct = 1
-prevct = 0
 dir = 1
+currX = x
 dist = 0
-while True:
-    newct = ct * dir;
-    for i in range(ct):
+for i in range(9):
+    for j in range(currX, x+(ct*dir)+1, dir):
         dist += 1
-        x += dir
-        if x == y:
-            print(i,file=w)
+        if(j == y):
+            print(dist,file=w)
+            f.close()
+            w.close()
             exit()
+    currX = x + (ct*dir)
     ct *= 2
-    dist += 1
     dir *= -1
     
-
+print(9 * abs(x-y),file=w)
 f.close()
 w.close()
