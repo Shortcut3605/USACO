c6ee5fc8eaea380c378cffdc4496a4032010ecaa
Shortcut3605 <breadzhacks@gmail.com>
2022-09-28 16:22:13 -0400

    Solved speeding

---
+++ speeding/speeding.py
@@ -0,0 +1,20 @@
+f = open("speeding.in","r")
+w = open("speeding.out","w")
+
+N, M = map(int, f.readline().strip().split())
+limits = []
+driving = []
+for i in range(N):
+    length, limit = map(int, f.readline().strip().split())
+    for j in range(length):
+        limits.append(limit)
+for i in range(M):
+    length, speed = map(int, f.readline().strip().split())
+    for j in range(length):
+        driving.append(speed)
+maxOver = 0
+for i in range(100):
+    maxOver = max(maxOver, driving[i]-limits[i])
+print(maxOver, file=w)
+f.close()
+w.close()
