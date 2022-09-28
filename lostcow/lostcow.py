bb6bbf6fd67f9775bb4483cbe01c29c4d2627815
Shortcut3605 <114258351+Shortcut3605@users.noreply.github.com>
2022-09-28 07:23:38 -0400

    Trying lostcow

---
+++ lostcow/lostcow.py
@@ -0,0 +1,18 @@
+f = open("lostcow.in", "r")
+w = open("lostcow.out","w")
+
+x,y = map(int, f.readline().strip().split())
+ct = 1
+prevct = 0
+dir = 1
+while True:
+    newct = ct * dir;
+    for i in range(ct):
+        x += dir
+        if x == y:
+            print(i,file=w)
+    ct *= 2
+    
+
+f.close()
+w.close()
